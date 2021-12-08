237f36a866c28e42c8985912a59b6a6ccfb58a94
MrRxdney (Rodney) <89966787+RodneyDaVinci@users.noreply.github.com>
2021-12-08 10:21:55 +0100

    Versie 1.1

--- papi-gelato.py
+++ papi-gelato.py
@@ -9,14 +9,14 @@ def stap1():
     keuze = ""
     aantal = int(input("Hoeveel bolletjes wilt u? "))
     if aantal <= 3:
-        stap2 = input(f"Wilt u deze {aantal} bolletje(s) in A) een hoorntje of B) een bakje? ")
+        stap2 = input(f"Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje? ")
     elif aantal <= 8:
         print(f"Dan krijgt u van mij een bakje met {aantal} bolletjes")
         stap1()
     elif aantal > 8:
         print("Sorry, zulke grote bakken hebben we niet")
         stap1()
-    keuze = input(f"Hier is uw {stap2} met {aantal} bolletje(s). Wilt u nog meer bestellen? (Y/N)")
+    keuze = input(f"Hier is uw {stap2} met {aantal} bolletje(s). Wilt u nog meer bestellen? (Y/N) ")
     if keuze == 'Y':
         stap1()
     elif keuze == 'N':
